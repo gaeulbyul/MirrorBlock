2a20a73d649860a880b165a24466a1c0ef8cd988
Gaeulbyul <aeiou256@gmail.com>
2018-07-22 21:00:55 +0900

    mirror-of-block.ts 작성

---
+++ src/options/options.js
@@ -0,0 +1,42 @@
+/* globals browser, ExtOption */
+
+const elements = {
+  outlineBlockUser: document.getElementById('outlineBlockUser'),
+  enableBlockReflection: document.getElementById('enableBlockReflection'),
+  chainBlockOver10KMode: document.getElementById('chainBlockOver10KMode'),
+  blockMutedUser: document.getElementById('blockMutedUser')
+}
+
+function saveOption () {
+  const option = {}
+  for (const key of Object.keys(elements)) {
+    option[key] = elements[key].checked
+  }
+  return ExtOption.save(option)
+}
+
+async function loadOption () {
+  const option = await ExtOption.load()
+  for (const key of Object.keys(elements)) {
+    elements[key].disabled = false
+    elements[key].checked = option[key]
+  }
+}
+
+function displayVersion () {
+  const elem = document.getElementById('version')
+  const manifest = browser.runtime.getManifest()
+  elem.textContent = manifest.version
+}
+
+function init () {
+  loadOption()
+  for (const input of document.querySelectorAll('.field input')) {
+    input.addEventListener('change', () => {
+      saveOption()
+    })
+  }
+  displayVersion()
+}
+
+document.addEventListener('DOMContentLoaded', init)
