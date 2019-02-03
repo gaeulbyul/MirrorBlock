d5e53155aa8cc784c213b661817330d852b2a370
Gaeulbyul <aeiou256@gmail.com>
2019-02-03 11:59:57 +0000

    모바일버전에서 다크모드 전환을 감지함

--- src/scripts/nightmode.ts
+++ src/scripts/nightmode.ts
@@ -1,15 +1,30 @@
 {
   const isDarkMode = /\bnight_mode=1\b/.test(document.cookie)
+  function toggleNightMode(isDarkMode: boolean): void {
+    document.documentElement.classList.toggle('mob-nightmode', isDarkMode)
+  }
 
   if (document.getElementById('react-root')) {
-    const colorThemeClass = isDarkMode ? 'mob-mobile-dark' : 'mob-mobile-light'
-    document.documentElement!.classList.add('mob-mobile', colorThemeClass)
-    document.documentElement.classList.toggle('mob-nightmode', isDarkMode)
-  } else {
-    function toggleNightMode(mode: boolean): void {
-      document.documentElement.classList.toggle('mob-nightmode', mode)
-    }
+    document.documentElement.classList.add('mob-mobile')
 
+    const colorThemeTag = document.querySelector('meta[name="theme-color"]')
+    if (colorThemeTag) {
+      const nightModeObserver = new MutationObserver(mutations => {
+        if (mutations.length <= 0) {
+          return
+        }
+        const target = mutations[0].target as HTMLMetaElement
+        const themeColor = target.content.toUpperCase()
+        const nightMode = themeColor !== '#FFFFFF'
+        toggleNightMode(nightMode)
+      })
+
+      nightModeObserver.observe(colorThemeTag, {
+        attributeFilter: ['content'],
+        attributes: true,
+      })
+    }
+  } else {
     const nightModeObserver = new MutationObserver(mutations => {
       for (const elem of getAddedElementsFromMutations(mutations)) {
         if (elem.matches('link.coreCSSBundles')) {
@@ -20,11 +35,11 @@
       }
     })
 
-    nightModeObserver.observe(document.head!, {
+    nightModeObserver.observe(document.head, {
       childList: true,
       subtree: true,
     })
-
-    toggleNightMode(isDarkMode)
   }
+
+  toggleNightMode(isDarkMode)
 }
