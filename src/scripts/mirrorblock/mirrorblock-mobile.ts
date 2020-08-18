230be1fc97c078b51f562481074796edb01610d0
Gaeulbyul <aeiou256@gmail.com>
2020-08-18 19:27:33 +0900

    멘션감지 등 수정
    이제 제대로 된다 ㅠㅠ

--- src/scripts/mirrorblock/mirrorblock-mobile.ts
+++ src/scripts/mirrorblock/mirrorblock-mobile.ts
@@ -16,192 +16,188 @@ function getElemByDmData(dmData: DMData): HTMLElement | null {
   return document.querySelector(`[data-mirrorblock-conversation-id="${dmData.conversation_id}"]`)
 }
 
-namespace ProfileDetector {
-  export async function detectProfile(rootElem: DOMQueryable) {
-    const helpLinks = rootElem.querySelectorAll<HTMLElement>(
-      'a[href="https://support.twitter.com/articles/20172060"]'
-    )
-    const helpLink = Array.from(Utils.iterateUntouchedElems(helpLinks)).shift()
-    if (!helpLink) {
-      return
+async function detectProfile(rootElem: DOMQueryable) {
+  const helpLinks = rootElem.querySelectorAll<HTMLElement>(
+    'a[href="https://support.twitter.com/articles/20172060"]'
+  )
+  const helpLink = Array.from(Utils.iterateUntouchedElems(helpLinks)).shift()
+  if (!helpLink) {
+    return
+  }
+  const userName = Utils.getUserNameFromTweetUrl(location)
+  if (!userName) {
+    return
+  }
+  // const user = StoreRetriever.getUserByName(userName)
+  const user = await TwitterAPI.getSingleUserByName(userName)
+  if (!user) {
+    return
+  }
+  const badge = new Badge(user)
+  reflectBlock({
+    user,
+    indicateBlock() {
+      badge.appendTo(helpLink.parentElement!)
+    },
+    indicateReflection() {
+      badge.blockReflected()
+      StoreUpdater.afterBlockUser(user)
+    },
+  })
+}
+
+// @ts-ignore TODO
+async function handleQuotedTweet(tweet: TweetWithQuote, tweetElem: HTMLElement) {
+  if (!tweet.quoted_status_permalink) {
+    return
+  }
+  const qUrlString = tweet.quoted_status_permalink.expanded
+  const qUrl = new URL(qUrlString)
+  // 드물게 인용 트윗 주소가 t.co 링크일 경우도 있더라.
+  if (qUrl.hostname === 't.co') {
+    return
+  }
+  const quotedUserName = Utils.getUserNameFromTweetUrl(qUrl)!
+  const quotedUser = await UserGetter.getUserByName(quotedUserName, true)
+  if (!quotedUser || !quotedUser.blocked_by) {
+    return
+  }
+  const badge = new Badge(quotedUser)
+  // 가끔 날 차단한 사람의 인용트윗이 보인다.
+  // 이 때 보이는 인용트윗은 div[role=blockquote]다.
+  // 안 보일 땐 트윗의 링크가 뜨는 데 그건 quoteLink
+  const quoteLink = tweetElem.querySelector(`a[href^="${qUrl.pathname}" i]`)
+  const quotedTweet = tweetElem.querySelector('div[role=blockquote]')
+  const indicateMe = quoteLink || quotedTweet || tweetElem
+  reflectBlock({
+    user: quotedUser,
+    indicateBlock() {
+      markOutline(indicateMe)
+      badge.attachAfter(indicateMe)
+    },
+    indicateReflection() {
+      badge.blockReflected()
+      StoreUpdater.afterBlockUser(quotedUser)
+    },
+  })
+}
+
+async function handleMentionsInTweet(tweet: Tweet, tweetElem: HTMLElement) {
+  const mentionedUserEntities = tweet.entities.user_mentions || []
+  if (mentionedUserEntities.length <= 0) {
+    return
+  }
+  const links = Array.from(tweetElem.querySelectorAll<HTMLAnchorElement>('a[role=link][href^="/"]'))
+  const mentionElemsMap = new Map<string, HTMLAnchorElement[]>(
+    mentionedUserEntities
+      .map(ent => ent.screen_name.toLowerCase())
+      .map((loweredName): [string, HTMLAnchorElement[]] => [
+        loweredName,
+        links.filter(a => a.pathname.toLowerCase() === `/${loweredName}`),
+      ])
+  )
+  const overflowed = tweetElem.querySelector('a[aria-label][href$="/people"]')
+  const mentionedUsersMap = await UserGetter.getMultipleUsersById(
+    mentionedUserEntities.map(u => u.id_str)
+  ).catch(err => {
+    console.error(err)
+    return null
+  })
+  if (!mentionedUsersMap) {
+    return
+  }
+  for (const mUser of mentionedUsersMap.values()) {
+    if (!mUser.blocked_by) {
+      continue
     }
-    const userName = Utils.getUserNameFromTweetUrl(location)
-    if (!userName) {
-      return
-    }
-    // const user = StoreRetriever.getUserByName(userName)
-    const user = await TwitterAPI.getSingleUserByName(userName)
-    if (!user) {
-      return
-    }
-    const badge = new Badge(user)
+    const loweredName = mUser.screen_name.toLowerCase()
+    const mentionElems = mentionElemsMap.get(loweredName)!
+    const badge = new Badge(mUser)
     reflectBlock({
-      user,
+      user: mUser,
       indicateBlock() {
-        badge.appendTo(helpLink.parentElement!)
+        if (mentionElems.length > 0) {
+          mentionElems.forEach(el => {
+            markOutline(el)
+            badge.attachAfter(el)
+          })
+        } else if (overflowed) {
+          markOutline(overflowed)
+          badge.showUserName()
+          badge.attachAfter(overflowed)
+        }
       },
       indicateReflection() {
         badge.blockReflected()
-        StoreUpdater.afterBlockUser(user)
+        StoreUpdater.afterBlockUser(mUser)
       },
     })
   }
 }
-namespace EntryHandler {
-  export async function handleQuotedTweet(tweet: TweetWithQuote, tweetElem: HTMLElement) {
-    if (!tweet.quoted_status_permalink) {
-      return
-    }
-    const qUrlString = tweet.quoted_status_permalink.expanded
-    const qUrl = new URL(qUrlString)
-    // 드물게 인용 트윗 주소가 t.co 링크일 경우도 있더라.
-    if (qUrl.hostname === 't.co') {
-      return
-    }
-    const quotedUserName = Utils.getUserNameFromTweetUrl(qUrl)!
-    const quotedUser = await UserGetter.getUserByName(quotedUserName, true)
-    if (!quotedUser || !quotedUser.blocked_by) {
-      return
-    }
-    const badge = new Badge(quotedUser)
-    // 가끔 날 차단한 사람의 인용트윗이 보인다.
-    // 이 때 보이는 인용트윗은 div[role=blockquote]다.
-    // 안 보일 땐 트윗의 링크가 뜨는 데 그건 quoteLink
-    const quoteLink = tweetElem.querySelector(`a[href^="${qUrl.pathname}" i]`)
-    const quotedTweet = tweetElem.querySelector('div[role=blockquote]')
-    const indicateMe = quoteLink || quotedTweet || tweetElem
-    reflectBlock({
-      user: quotedUser,
-      indicateBlock() {
-        markOutline(indicateMe)
-        badge.attachAfter(indicateMe)
-      },
-      indicateReflection() {
-        badge.blockReflected()
-        StoreUpdater.afterBlockUser(quotedUser)
-      },
-    })
-  }
-  export async function handleMentionsInTweet(tweet: Tweet, tweetElem: HTMLElement) {
-    const mentionedUserEntities = tweet.entities.user_mentions || []
-    if (mentionedUserEntities.length <= 0) {
-      return
-    }
-    const links = Array.from(
-      tweetElem.querySelectorAll<HTMLAnchorElement>('a[role=link][href^="/"]')
-    )
-    const mentionElemsMap = new Map<string, HTMLAnchorElement[]>(
-      mentionedUserEntities
-        .map(ent => ent.screen_name.toLowerCase())
-        .map((loweredName): [string, HTMLAnchorElement[]] => [
-          loweredName,
-          links.filter(a => a.pathname.toLowerCase() === `/${loweredName}`),
-        ])
-    )
-    const overflowed = tweetElem.querySelector('a[aria-label][href$="/people"]')
-    const mentionedUsers = await StoreRetriever.getMultipleUsersByIds(
-      mentionedUserEntities.map(u => u.id_str)
-    )
-    /*
-    const mentionedUsersMap = await UserGetter.getMultipleUsersById(
-      mentionedUserEntities.map(u => u.id_str)
-    ).catch(() => null)
-    */
-    for (const mUser of Object.values(mentionedUsers)) {
-      if (!mUser.blocked_by) {
-        continue
-      }
-      const loweredName = mUser.screen_name.toLowerCase()
-      const mentionElems = mentionElemsMap.get(loweredName)!
-      const badge = new Badge(mUser)
-      reflectBlock({
-        user: mUser,
-        indicateBlock() {
-          if (mentionElems.length > 0) {
-            mentionElems.forEach(el => {
-              markOutline(el)
-              badge.attachAfter(el)
-            })
-          } else if (overflowed) {
-            markOutline(overflowed)
-            badge.showUserName()
-            badge.attachAfter(overflowed)
-          }
-        },
-        indicateReflection() {
-          badge.blockReflected()
-          StoreUpdater.afterBlockUser(mUser)
-        },
-      })
-    }
-  }
+
+async function handleUserCells(user: TwitterUser, elem: HTMLElement) {
+  const badge = new Badge(user)
+  reflectBlock({
+    user,
+    indicateBlock() {
+      markOutline(elem)
+      const badgeTarget = elem.querySelector('div[dir=ltr]')!
+      badge.attachAfter(badgeTarget)
+    },
+    indicateReflection() {
+      badge.blockReflected()
+      StoreUpdater.afterBlockUser(user)
+    },
+  })
 }
-namespace UserCellHandler {
-  export async function handleUserCells(user: TwitterUser, elem: HTMLElement) {
-    const badge = new Badge(user)
-    reflectBlock({
-      user,
+
+async function handleDMConversation(convId: string) {
+  const dmData = await StoreRetriever.getDMData(convId)
+  if (!dmData) {
+    throw new Error('unreachable')
+  }
+  if (!dmData.read_only) {
+    return
+  }
+  const elem = getElemByDmData(dmData)!
+  const badgeTarget = elem.querySelector('div[dir=ltr]')!
+  const participants = await UserGetter.getMultipleUsersById(
+    dmData.participants.map(par => par.user_id)
+  )
+  const blockedMe = participants.filter(user => !!user.blocked_by)
+  if (blockedMe.size <= 0) {
+    return
+  }
+  for (const userToBlock of blockedMe.values()) {
+    const badge = new Badge(userToBlock)
+    if (dmData.type === 'GROUP_DM') {
+      badge.showUserName()
+    }
+    await reflectBlock({
+      user: userToBlock,
       indicateBlock() {
         markOutline(elem)
-        const badgeTarget = elem.querySelector('div[dir=ltr]')!
-        badge.attachAfter(badgeTarget)
+        badge.appendTo(badgeTarget)
       },
       indicateReflection() {
         badge.blockReflected()
-        StoreUpdater.afterBlockUser(user)
+        StoreUpdater.afterBlockUser(userToBlock)
       },
     })
-  }
-}
-namespace DMHandler {
-  export async function handleDMConversation(convId: string) {
-    const dmData = await StoreRetriever.getDMData(convId)
-    if (!dmData) {
-      throw new Error('unreachable')
-    }
-    if (!dmData.read_only) {
-      return
-    }
-    const elem = getElemByDmData(dmData)!
-    const badgeTarget = elem.querySelector('div[dir=ltr]')!
-    const participants = await UserGetter.getMultipleUsersById(
-      dmData.participants.map(par => par.user_id)
-    )
-    const blockedMe = participants.filter(user => !!user.blocked_by)
-    if (blockedMe.size <= 0) {
-      return
-    }
-    for (const userToBlock of blockedMe.values()) {
-      const badge = new Badge(userToBlock)
-      if (dmData.type === 'GROUP_DM') {
-        badge.showUserName()
-      }
-      await reflectBlock({
-        user: userToBlock,
-        indicateBlock() {
-          markOutline(elem)
-          badge.appendTo(badgeTarget)
-        },
-        indicateReflection() {
-          badge.blockReflected()
-          StoreUpdater.afterBlockUser(userToBlock)
-        },
-      })
-    }
   }
 }
 
 const promisesQueue = new (class {
   private promise = Promise.resolve()
-  public push(newPromise: Promise<void>) {
-    this.promise = this.promise.then(() => newPromise)
+  public push(newAsyncFunc: () => Promise<void>) {
+    this.promise = this.promise.then(newAsyncFunc)
   }
 })()
 
 function startObserve(reactRoot: HTMLElement): void {
   new MutationObserver(mutations => {
     for (const elem of Utils.getAddedElementsFromMutations(mutations)) {
-      ProfileDetector.detectProfile(elem)
+      detectProfile(elem)
     }
   }).observe(reactRoot, {
     subtree: true,
@@ -211,18 +207,18 @@ function startObserve(reactRoot: HTMLElement): void {
     const customEvent = event as CustomEvent
     const elem = customEvent.target as HTMLElement
     const { user } = customEvent.detail
-    UserCellHandler.handleUserCells(user, elem)
+    handleUserCells(user, elem)
   })
   document.addEventListener('MirrorBlock<-DMConversation', event => {
     const customEvent = event as CustomEvent<{ convId: string }>
     const { convId } = customEvent.detail
-    DMHandler.handleDMConversation(convId)
+    handleDMConversation(convId)
   })
   document.addEventListener('MirrorBlock<-Tweet', event => {
     const customEvent = event as CustomEvent
     const elem = customEvent.target as HTMLElement
     const { tweet } = customEvent.detail
-    promisesQueue.push(EntryHandler.handleMentionsInTweet(tweet, elem))
+    promisesQueue.push(() => handleMentionsInTweet(tweet, elem))
   })
 }
 async function isLoggedIn(): Promise<boolean> {
